adb64629a948331ee85692bc1d4832a445ab07c3
你的 Github 用户名 < Github >
2024-07-05 23:22:05 +0800

    Site updated: 2024-07-05 23:22:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/05/hello-world/"];function toRandomPost(){
+var posts=["2023/12/05/2023-ZJ-noip-迷惑行为大赏/","2024/07/05/hello-world/","2024/05/08/树状数组/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
