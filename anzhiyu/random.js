1e6e4ceae013001f0552409219eab4839ff418ad
你的 Github 用户名 < Github >
2024-07-08 11:56:35 +0800

    Site updated: 2024-07-08 11:56:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/a8eee40e/","posts/a996222c/","posts/ffca3de1/","posts/b0170d0e/","posts/59a0de58/","posts/362ffdf7/"];function toRandomPost(){
+var posts=["posts/ffca3de1/","posts/b0170d0e/","posts/a8eee40e/","posts/a996222c/","posts/362ffdf7/","posts/59a0de58/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
