a73a5c0ccc97b2fe682da05b70fdc9f6e069b1e7
你的 Github 用户名 < Github >
2025-06-09 11:52:04 +0800

    Site updated: 2025-06-09 11:51:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/ffca3de1/","posts/1059/","posts/44007/","posts/48555/","posts/59a0de58/"];function toRandomPost(){
+var posts=["posts/ffca3de1/","posts/1059/","posts/59a0de58/","posts/48555/","posts/44007/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
